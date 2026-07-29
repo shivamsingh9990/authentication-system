413d168ab0a29d5c83d5c23a57481dd6681ee8e6
shivam <shivamsinghmaurya9@gmail.com>
2026-07-30 00:13:28 +0530

    import

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
+const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 export default function Signup() {
   const [formData, setFormData] = useState({
